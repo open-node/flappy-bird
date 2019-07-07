37db8894b92e709129da4570b1f49446aabd0633
Redstone Zhao <zhaoxiongfei@admaster.com.cn>
2019-07-08 00:58:04 +0800

    完成background 的改造，极大地简化了代码

--- dist/main.js
+++ dist/main.js
@@ -106,23 +106,15 @@ module.exports = Actor;
 },{}],3:[function(require,module,exports){
 const Actor = require("./actor");
 
+const n = ((Math.random() * 7919) | 0) % 2;
+
 class Background extends Actor {
   reset() {
     this.x = 0;
     this.stop = true;
-    this.oX = 0;
-    this.oY = 0;
     this.w = 285;
     this.h = 510;
     this.lackH = 130;
-  }
-
-  constructor(game) {
-    super(game);
-
-    // 随机选择白天or黑夜的背景图
-    // 这里的 X 计算的是切片的x位移量
-    this.oX = 293 * (((Math.random() * 7919) | 0) % 2);
   }
 
   update() {
@@ -132,16 +124,15 @@ class Background extends Actor {
   }
 
   render() {
-    const { ctx, img } = this.game;
     // 收尾详解三个切面图
-    ctx.drawImage(img, this.oX, this.oY, this.w, this.h, this.x, this.lackH, this.w, this.h);
-    ctx.drawImage(img, this.oX, this.oY, this.w, this.h, this.x + this.w, this.lackH, this.w, this.h);
-    ctx.drawImage(img, this.oX, this.oY, this.w, this.h, this.x + this.w * 2, this.lackH, this.w, this.h);
+    this.game.drawImageByName(`bg_${n}`, this.x, this.lackH);
+    this.game.drawImageByName(`bg_${n}`, this.x + this.w, this.lackH);
+    this.game.drawImageByName(`bg_${n}`, this.x + this.w * 2, this.lackH);
 
     // 补充头部确实的区域
-    ctx.drawImage(img, this.oX, this.oY, this.w, this.lackH, this.x, 0, this.w, this.lackH);
-    ctx.drawImage(img, this.oX, this.oY, this.w, this.lackH, this.x + this.w, 0, this.w, this.lackH);
-    ctx.drawImage(img, this.oX, this.oY, this.w, this.lackH, this.x + this.w * 2, 0, this.w, this.lackH);
+    this.game.drawImageByName(`sky_${n}`, this.x, 0);
+    this.game.drawImageByName(`sky_${n}`, this.x + this.w, 0);
+    this.game.drawImageByName(`sky_${n}`, this.x + this.w * 2, 0);
   }
 }
 
